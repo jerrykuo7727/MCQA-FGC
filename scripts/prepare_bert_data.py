90d15b39c5ae68d8f4280189d3b6492662d54623
Jiazhi Guo <jerrykuo7727@gmail.com>
2020-02-28 05:49:05 +0800

    Fix bug of seperating multi-choice which causes autograd failure.

--- scripts/prepare_bert_data.py
+++ scripts/prepare_bert_data.py
@@ -3,6 +3,7 @@ import json
 import torch
 from os.path import join, exists
 from transformers import BertTokenizer
+from torch.nn.utils.rnn import pad_sequence
 
 
 if __name__ == '__main__':
@@ -22,11 +23,13 @@ if __name__ == '__main__':
         for i, PQCA in enumerate(data):
             p_tokens = tokenizer.tokenize(PQCA['passage']) + [tokenizer.sep_token]
             q_tokens = tokenizer.tokenize(PQCA['question']) + [tokenizer.sep_token]
-            answer = torch.LongTensor([PQCA['answer']]).squeeze(0)
+            answer = torch.LongTensor([PQCA['answer'] - 1]).squeeze(0)
             torch.save(answer, 'data/%s/answer/%s|%s.pt' % (split, dataset, i))
             
             # Individually process QCP input
             all_qcp_ids = []
+            all_attention_mask = []
+            all_token_type_ids = []
             for ci, key in enumerate(['c1', 'c2', 'c3', 'c4'], start=1):
                 c_tokens = tokenizer.tokenize(PQCA[key]) + [tokenizer.sep_token]
                 qc_tokens = q_tokens + c_tokens
@@ -36,11 +39,20 @@ if __name__ == '__main__':
                 qcp_ids = torch.LongTensor(tokenizer.convert_tokens_to_ids(qcp_tokens))
                 attention_mask = torch.FloatTensor([1 for token in qcp_tokens])
                 token_type_ids = torch.LongTensor([0, *(0 for _ in qc_tokens), *(1 for _ in p_tokens)])
+                assert qcp_ids.shape == attention_mask.shape == token_type_ids.shape
                 
-                # Save tensors
-                torch.save(qcp_ids, 'data/%s/qcp_%d/%s|%s.pt' % (split, ci, dataset, i))
-                torch.save(attention_mask, 'data/%s/amask_%d/%s|%s.pt' % (split, ci, dataset, i))
-                torch.save(token_type_ids, 'data/%s/ttype_%d/%s|%s.pt' % (split, ci, dataset, i))
+                # Stack tensors
+                all_qcp_ids.append(qcp_ids)
+                all_attention_mask.append(attention_mask)
+                all_token_type_ids.append(token_type_ids)
+                
+            # Pad and save tensors
+            qcp_ids = pad_sequence(all_qcp_ids, batch_first=True)
+            attention_mask = pad_sequence(all_attention_mask, batch_first=True)
+            token_type_ids = pad_sequence(all_token_type_ids, batch_first=True)
+            torch.save(qcp_ids, 'data/%s/qcp/%s|%s.pt' % (split, dataset, i))
+            torch.save(attention_mask, 'data/%s/amask/%s|%s.pt' % (split, dataset, i))
+            torch.save(token_type_ids, 'data/%s/ttype/%s|%s.pt' % (split, dataset, i))
 
             print('%s: %d/%d (%.2f%%) \r' % (dataset, i+1, n_data, 100*(i+1)/n_data), end='')
     print()
